cff6aa456688e27c2376cd2bb0fa55a6e4a8ecbe
Wacharakorn.p <sloth.devtaken@gmail.com>
2024-05-21 22:23:05 +0700

    add test2

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -34,6 +35,43 @@ func TestRootHandler(t *testing.T) {
 
 	expected := "OK"
 	if string(body) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			string(body), expected)
+	}
+}
+
+func TestStatusHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/status", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]string{"status": "running"}
+		json.NewEncoder(w).Encode(response)
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	resp := rr.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+
+	expected := `{"status":"running"}`
+	var actual map[string]string
+	if err := json.Unmarshal(body, &actual); err != nil {
+		t.Fatalf("could not unmarshal response: %v", err)
+	}
+	if actual["status"] != "running" {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			string(body), expected)
 	}
