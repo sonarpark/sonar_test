5c1b9319f63258a019348005476038f3364b9594
wacharakorn.p <sloth.devtaken@gmail.com>
2024-06-06 18:42:08 +0700

    test webhook

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 		fmt.Fprintf(w, "OK")
 	})
 
+	http.HandleFunc("/test-1", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "OK")
+	})
+
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
