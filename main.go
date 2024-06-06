3c62af5a6fbb376bac5f567f669bb047361cae24
wacharakorn.p <sloth.devtaken@gmail.com>
2024-06-06 18:48:47 +0700

    test2

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ func main() {
 		fmt.Fprintf(w, "OK")
 	})
 
+	http.HandleFunc("/test-2", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "OK")
+	})
+
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
